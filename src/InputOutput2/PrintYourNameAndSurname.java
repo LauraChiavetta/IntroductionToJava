8781c6fff0d8a70f0463bc702d484f03778c612f
Laura Chiavetta <chiavetta.laura18@gmail.com>
2023-06-07 19:08:49 +0200

    Input And Output 2 added

---
+++ src/InputOutput2/PrintYourNameAndSurname.java
@@ -0,0 +1,8 @@
+package InputOutput2;
+
+public class PrintYourNameAndSurname {
+    public static void main(String[] args) {
+        System.out.print("Laura");
+        System.out.print(" Chiavetta");
+    }
+}
