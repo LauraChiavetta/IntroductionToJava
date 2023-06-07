dadcd7c2ab13d241ce4e146e2b02a6dc6a55c80e
Laura Chiavetta <chiavetta.laura18@gmail.com>
2023-06-07 19:24:06 +0200

    Number Operations added

---
+++ src/NumberOperations/Testing.java
@@ -0,0 +1,14 @@
+package NumberOperations;
+
+public class Testing {
+    public static void main(String[] args) {
+        int a = 15;
+        a +=5;
+        a-=4;
+        int b = a;
+        b++;
+        System.out.println(a);
+        System.out.println(b*(b+1));
+
+    }
+}
