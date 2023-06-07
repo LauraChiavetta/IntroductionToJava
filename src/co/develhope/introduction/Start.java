3722dc92164324a00a1066a277f0015a15595170
Laura Chiavetta <chiavetta.laura18@gmail.com>
2023-06-07 18:48:02 +0200

    First exercise added

---
+++ src/co/develhope/introduction/Start.java
@@ -0,0 +1,8 @@
+package co.develhope.introduction;
+
+public class Start {
+    public static void main(String[] args) {
+        System.out.println("Hello develhope java students");
+        System.out.println("My birthday is on 18th of january!");
+    }
+}
