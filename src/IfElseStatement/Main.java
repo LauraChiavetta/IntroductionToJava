0a6415b8717b72540b4cd9b31bd7263fe8f70ce0
Laura Chiavetta <chiavetta.laura18@gmail.com>
2023-06-08 11:36:21 +0200

    If else statement added

---
+++ src/IfElseStatement/Main.java
@@ -0,0 +1,12 @@
+package IfElseStatement;
+
+public class Main {
+    public static void main(String[] args) {
+        String myName = "Laura";
+        if(myName.length()%2==0){
+            System.out.println("Your name is even");
+        } else{
+            System.out.println("Your name is odd");
+        }
+    }
+}
